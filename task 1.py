b34593fbec6955ae1812c549eb7dd1e02c522574
Biswajit4027 <biswajitsahauit@gmail.com>
2025-06-14 09:26:57 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a=int(input("enter num_1 : "))
+b=int(input("enter num_2 : "))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(f"the sum of {a} and {b} is : {add}")
+print(f"the substraction of {b} from {a} is : {sub}")
+print(f"the product of {a} and {b} is : {mul}")
+print(f"the quotient of {a} divided by {b} is : {div:.2f}")
---
+++ task2.py
@@ -0,0 +1,6 @@
+#TASK 2: CREATE A PERSONALIZED GREETING!
+
+first_name=input("Enter your First Name : ")
+last_name=input("Enter your Last Name : ")
+full_name=first_name+" "+last_name
+print(f"Hello, {full_name}! Welcome to Python program.")
